cdd983ff12d96d75023b204f7024d855ee9917c0
caisar <82203475+caisar@users.noreply.github.com>
2024-10-11 19:35:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";window.buffer=window.Buffer;const e=new xrpIO("wss://s.altnet.rippletest.net:51233");e.connect().then((async t=>{const n=await e.treeRead(["7B54A97187F3D03DA54C63A46C3C21C9147CE11877A71D852AF79B61D1A5F303"]);document.write(n)}))})();
+(()=>{"use strict";window.buffer=window.Buffer;const e=new xrpIO("wss://s.altnet.rippletest.net:51233");e.connect().then((async t=>{const n=await e.treeRead(["4055580294573634A592117354E181F0D6B8EBF2B57066DCA186AA007968376B"]);document.write(n)}))})();
