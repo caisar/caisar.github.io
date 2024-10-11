68cff5d67a4a0f8f94f3d59095f6b7116baa7c0e
caisar <82203475+caisar@users.noreply.github.com>
2024-10-11 19:10:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";window.buffer=window.Buffer;const e=new xrpIO("wss://s.altnet.rippletest.net:51233");e.connect().then((async t=>{const n=await e.treeRead(["1FFF88988B52C06B81E27086F3A75574D9E8C39CDFF7076EFB03C38C32473DF4"]);document.write(n)}))})();
+(()=>{"use strict";window.buffer=window.Buffer;const e=new xrpIO("wss://s.altnet.rippletest.net:51233");e.connect().then((async t=>{const n=await e.treeRead(["7B54A97187F3D03DA54C63A46C3C21C9147CE11877A71D852AF79B61D1A5F303"]);document.write(n)}))})();
